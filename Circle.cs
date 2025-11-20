4a8e029e7986fe96b886ed1eb8902e7e8c174843
brianykak24-netizen <brianyk_ak24@nuwm.edu.ua>
2025-11-20 02:21:02 +0200

    qwerty

--- Circle.cs
+++ Circle.cs
@@ -8,57 +8,57 @@ namespace LabWork
 {
     public class Circle
     {
-        private int x_1, x_2, x_3, b_1, b_2, b_3, R;
+        private double X_1, X_2, X_3, B_1, B_2, B_3, R;
 
         public Circle()
         {
             //fill this ctor
-            x_1 = 0;
-            x_2 = 0;
-            x_3 = 0;
-            b_1 = 0;
-            b_2 = 0;
-            b_3 = 0;
+            X_1 = 0;
+            X_2 = 0;
+            X_3 = 0;
+            B_1 = 0;
+            B_2 = 0;
+            B_3 = 0;
             R = 1;
                
         }
-        public Circle(int x_1, int x_2, int x_3, int b_1, int b_2, int b_3, int R)
+        public Circle(double x_1, double x_2, double x_3, double b_1, double b_2, double b_3, double R)
         {
-            this.x_1 = x_1;
-            this.x_2 = x_2;
-            this.x_3 = x_3;
+            this.X_1 = x_1;
+            this.X_2 = x_2;
+            this.X_3 = x_3;
 
-            this.b_1 = b_1;
-            this.b_2 = b_2;
-            this.b_3 = b_3;
+            this.B_1 = b_1;
+            this.B_2 = b_2;
+            this.B_3 = b_3;
             
             this.R = R;
         }
 
-        public int GetR() { return R; }
-        public int GetX_1() { return x_1; }
-        public int GetX_2() { return x_2; }
-        public int GetX_3() { return x_3; }
+        public double GetR() { return R; }
+        public double GetX_1() { return X_1; }
+        public double GetX_2() { return X_2; }
+        public double GetX_3() { return X_3; }
 
-        public int GetB_1() { return b_1; }
-        public int GetB_2() { return b_2; }
-        public int GetB_3() { return b_3; }
+        public double GetB_1() { return B_1; }
+        public double GetB_2() { return B_2; }
+        public double GetB_3() { return B_3; }
 
 
         public virtual double GetVolume() { return (double)(4.0 / 3.0) * Math.PI * R * R * R; }
-        public override string ToString() {return $"({x_1} - {b_1})^2 - ({x_2} - {b_2})^2 + ({x_3} - {b_3})^2 = ({R})^2";}
+        public override string ToString() {return $"({X_1} - {B_1})^2 - ({X_2} - {B_2})^2 + ({X_3} - {B_3})^2 = ({R})^2";}
 
-        public void FillData()
+        public virtual void FillData()
         {
             Console.WriteLine("Enter R >>> "); R = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter x_1 >>> "); x_1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter x_2 >>> "); x_2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter x_3 >>> "); x_3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter X_1 >>> "); X_1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter X_2 >>> "); X_2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter X_3 >>> "); X_3 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter b_1 >>> "); b_1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter b_2 >>> "); b_2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter b_3 >>> "); b_3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter B_1 >>> "); B_1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter B_2 >>> "); B_2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter B_3 >>> "); B_3 = Convert.ToInt32(Console.ReadLine());
 
         }
 
--- Ellipsoid.cs
+++ Ellipsoid.cs
@@ -8,35 +8,36 @@ namespace LabWork
 {
     public class Ellipsoid : Circle
     {
-        private int a_1, a_2, a_3;
-        public Ellipsoid(int x_1, int x_2, int x_3, int b_1, int b_2, int b_3, int a_1, int a_2, int a_3, int R = 1) : base(x_1, x_2, x_3, b_1, b_2, b_3, R)
+        private double A_1, A_2, A_3;
+        public Ellipsoid(double x_1, double x_2, double x_3, double b_1, double b_2, double b_3, double a_1, double a_2, double a_3, double R = 1)
+            : base(x_1, x_2, x_3, b_1, b_2, b_3, R)
         {
-            this.a_1 = a_1;
-            this.a_2 = a_2;
-            this.a_3 = a_3;
+            this.A_1 = a_1;
+            this.A_2 = a_2;
+            this.A_3 = a_3;
         }
 
         public Ellipsoid() : base()
         {
-            this.a_1 = 0;
-            this.a_2 = 0;
-            this.a_3 = 0;
+            this.A_1 = 0;
+            this.A_2 = 0;
+            this.A_3 = 0;
         }
 
-        public int GetA_1() { return a_1; }
-        public int GetA_2() { return a_2; }
-        public int GetA_3() { return a_3; }
+        public double GetA_1() { return A_1; }
+        public double GetA_2() { return A_2; }
+        public double GetA_3() { return A_3; }
 
-        public override double GetVolume() { return (double)(4.0 / 3.0) * Math.PI * (a_1) * (a_2) * (a_3); }
-        public override string ToString() { return $"(({GetX_1()} - {GetB_1()})^2)/({a_1}^2) + (({GetX_2()} - {GetB_2()})^2)/({a_2}^2) + (({GetX_3()} - {GetB_3()})^2)/({a_3}^2) = 1"; }
+        public override double GetVolume() { return (double)(4.0 / 3.0) * Math.PI * (A_1) * (A_2) * (A_3); }
+        public override string ToString() { return $"(({GetX_1()} - {GetB_1()})^2)/({A_1}^2) + (({GetX_2()} - {GetB_2()})^2)/({A_2}^2) + (({GetX_3()} - {GetB_3()})^2)/({A_3}^2) = 1"; }
 
-        public void FillData()
+        public override void FillData()
         {
             base.FillData();
 
-            Console.WriteLine("Enter a_1 >>> "); a_1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter a_2 >>> "); a_2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter a_3 >>> "); a_3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter A_1 >>> "); A_1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter A_2 >>> "); A_2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter A_3 >>> "); A_3 = Convert.ToInt32(Console.ReadLine());
         }
     }
 }
